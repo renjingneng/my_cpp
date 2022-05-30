19df71f5024bf6d286daee2cc2ad9d100746432e
renjingneng <renjingneng@chinamcloud.com>
2022-05-30 20:07:14 +0800

    test

--- modernC++/advanced.cpp
+++ modernC++/advanced.cpp
@@ -40,6 +40,59 @@ public:
 };
 Human::Human(string name, int age) : name(name), age(age) {}
 
+
+//if constexpr
+//https://stackoverflow.com/questions/59053142/c-template-function-cant-convert-stdstring-to-double-in-return
+template <typename T>
+string convert(T input) {
+    if constexpr (is_same_v<T, const char*> ||
+        is_same_v<T, std::string>) {
+        return input;
+    }
+    else {
+        return to_string(input);
+    }
+}
+
+/****
+A nested-type object contains only the members defined inside the nested type. 
+Similarly, an object of the enclosing class has only those members that are defined by the enclosing class. 
+Enclosing class does not contain the data members of any nested classes.
+****/
+//unordered_set for user-defined class
+class Point
+{
+public:
+
+    Point() { }
+    Point(int x, int y)
+    {
+        this->x = x;
+        this->y = y;
+    }
+
+    struct HashFunction
+    {
+        size_t operator()(const Point& point) const
+        {
+            std::hash<int> intHasher;
+            size_t xHash = intHasher(point.x);
+            size_t yHash = intHasher(point.y) << 1;
+            return xHash ^ yHash;
+        }
+    };
+    bool operator==(const Point& otherPoint) const
+    {
+        if (this->x == otherPoint.x && this->y == otherPoint.y) return true;
+        else return false;
+    }
+
+    int x;
+    int y;
+};
+
+
+
 int main()
 {
 	// variadic templates in C++11 and fold expressions in C++17
@@ -73,6 +126,22 @@ int main()
     while (!Q.empty()) {
         cout << "Name: " << Q.top().name << ", age: " << Q.top().age << endl;
         Q.pop();
+    }
+
+    //if constexpr
+    cout<<convert(2)<<endl;
+    cout<<convert("222") << endl;
+    cout << convert(string("2222")) << endl;
+
+    //unordered_set
+    std::unordered_set<Point, Point::HashFunction> points;
+    points.insert(Point(1, 1));
+    points.insert(Point(2, 2));
+    points.insert(Point(1, 1));   // notice this is a duplicate with the 1st point so it won't change the set
+    std::cout << "points: " << "\n";
+    for (auto& point : points)
+    {
+        std::cout << "(" << point.x << ", " << point.y << ")" << "\n";
     }
 	return 0;
 }
